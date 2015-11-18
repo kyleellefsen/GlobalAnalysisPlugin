064b4717611d6805d2ecf67c97326d35e4dd70cd
Brett Settle <brettjsettle@gmail.com>
2015-11-18 03:21:34 -0800

    date change

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 name = 'Global Analysis'
 base_dir = 'GlobalAnalysis'
-date = '11/15/2015'
+date = '11/19/2015'
 dependencies = ['xlrd']
 menu_layout = {'Global Analysis': ['GlobalAnalysis', 'gui']}
