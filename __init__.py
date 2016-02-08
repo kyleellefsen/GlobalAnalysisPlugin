16bcbf1e38bb2cdb23fd26af6303746afa325aa3
Brett Settle <brettjsettle@gmail.com>
2016-02-08 13:49:31 -0800

    init finished

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
-{
-'base_dir': 'GlobalAnalysis',
+{'base_dir': 'GlobalAnalysis',
 'author': 'Brett Settle',
 'description':'Analyze the fall and rise times of ROIs by fitting the average trace to a polynomial',
+'url': 'http://github.com/BrettJSettle/GlobalAnalysisPlugin/archive/master.zip',
 'date': '2/8/2016',
 'dependencies': [],
 'menu_layout': [('Global Analysis', ['GlobalAnalysis', 'gui'])]}
