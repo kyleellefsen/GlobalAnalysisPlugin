4a1b4d8c722d87b8b1119151be43f816ce6f3242
Brett Settle <brettjsettle@gmail.com>
2016-02-08 13:32:55 -0800

    init change

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 name = 'Global Analysis'
 base_dir = 'GlobalAnalysis'
+author: 'Brett Settle'
+description = 'Analyze the fall and rise times of ROIs by fitting the average trace to a polynomial'
 date = '2/8/2016'
 dependencies = []
 menu_layout = {'Global Analysis': ['GlobalAnalysis', 'gui']}
