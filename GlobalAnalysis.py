4dab26a4a4f789f2b887dad65f3435f2caadb952
Brett Settle <brettjsettle@gmail.com>
2016-02-08 13:16:42 -0800

    plugin works

--- GlobalAnalysis.py
+++ GlobalAnalysis.py
@@ -15,56 +15,60 @@ def gui():
 		analysisUI = uic.loadUi(os.path.join(os.getcwd(), 'plugins\\GlobalAnalysis\\main.ui'))
 		analysisUI.traceROICheck.toggled.connect(toggleVisible)
 		traceRectROI.sigRegionChanged.connect(fillDataTable)
-		QApplication.instance().focusChanged.connect(focusChange)
 		analysisUI.measureButton.clicked.connect(measure.gui)
 		analysisUI.closeEvent = closeEvent
+		analysisUI.tableWidget.setFormat("%.3f")
+		analysisUI.traceComboBox.mousePressEvent = comboBoxClicked
 		g.m.dialogs.append(analysisUI)
+		analysisUI.traceComboBox.currentIndexChanged.connect(indexChanged)
 	analysisUI.show()
+
+
+
+def indexChanged(i):
+	if analysisUI.traceComboBox.updating or i == -1 or i >= len(analysisUI.all_rois):
+		return
+	traceRectROI.setTrace(analysisUI.all_rois[i]['p1trace'])
 
 def closeEvent(ev):
 	if g.m.currentTrace != None:
 		g.m.currentTrace.p1.removeItem(traceRectROI)
-	#traceRectROI.parentItem().removeItem(traceRectROI)
 	ev.accept()
 
 def toggleVisible(v):
 	traceRectROI.setVisible(v)
-	if g.m.currentTrace != None:
-		g.m.currentTrace.p1.addItem(traceRectROI)
-	buildComboBox()
+	if not v:
+		analysisUI.tableWidget.clear()
+	else:
+		fillDataTable()
 
 def buildComboBox():
+	analysisUI.traceComboBox.updating = True
 	analysisUI.traceComboBox.clear()
-	if not g.m.currentTrace or len(g.m.currentTrace.rois) == 0:
+	analysisUI.traceWindows = []
+	for traceWindow in g.m.traceWindows:
+		analysisUI.all_rois.extend(traceWindow.rois)
+	if len(analysisUI.all_rois) == 0:
 		analysisUI.traceComboBox.addItem("No Trace Selected")
-		return
-	model = analysisUI.traceComboBox.model()
-	traceRectROI.traces = [None]
-	for i, roiLine in enumerate(g.m.currentTrace.rois):
-		traceRectROI.traces.append(roiLine['p1trace'])
-		item = QStandardItem("ROI #%d" % (i + 1))
-		item.setBackground(roiLine['roi'].color)
-		model.appendRow(item)
-		if roiLine['p1trace'] == traceRectROI.traces[i]:
-			traceComboBox.setSelectedItem(i)
-	analysisUI.traceComboBox.currentIndexChanged.connect(lambda v: traceRectROI.setTrace(traceRectROI.traces[v]))
+	else:
+		model = analysisUI.traceComboBox.model()
+		for i, roiLine in enumerate(analysisUI.all_rois):
+			item = QStandardItem("ROI #%d" % (i + 1))
+			item.setBackground(roiLine['roi'].color)
+			model.appendRow(item)
+			if roiLine['p1trace'] == traceRectROI.traceLine:
+				analysisUI.traceComboBox.setCurrentIndex(i)
+	analysisUI.traceComboBox.updating = False
 
-def focusChange(old, new):
-	if new != None and isinstance(new.window(), TraceFig) and new != g.m.currentTrace:
-		if old != None and isinstance(old, TraceFig):
-			old.window().p1.removeItem(traceRectROI)
-		if traceRectROI not in new.window().p1.getPlotItem().listDataItems():
-			new.window().p1.addItem(traceRectROI)
+def comboBoxClicked(ev):
+	buildComboBox()
+	QComboBox.mousePressEvent(analysisUI.traceComboBox, ev)
 
 def fillDataTable():
 	''' when the region moves, recalculate the polyfit
 	data and plot/show it in the table and graph accordingly'''
-	t = traceRectROI.getFrameTrace()
-	if not t:
+	if not traceRectROI.traceLine:
 		return
-	x, y = t
-	ftrace = get_polyfit(x, y)
-	data = traceRectROI.analyze_trace()
-	analysisUI.tableWidget.setData(data)
+	analysisUI.tableWidget.setData(traceRectROI.data)
 	analysisUI.tableWidget.setHorizontalHeaderLabels(['Frames', 'Y', 'Ftrace Frames', 'Ftrace Y'])
 	
