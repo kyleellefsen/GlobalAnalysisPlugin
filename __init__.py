5defd2adc4578bd49d0fe9fd8ef624200cf800a3
Brett Settle <brettjsettle@gmail.com>
2016-02-09 21:55:02 -0800

    new init

--- __init__.py
+++ __init__.py
@@ -1,8 +1,30 @@
-{'name': 'Global Analysis',
-'base_dir': 'GlobalAnalysis',
-'author': 'Brett Settle',
-'description':'Analyze the fall and rise times of ROIs by fitting the average trace to a polynomial',
-'url': 'http://github.com/BrettJSettle/GlobalAnalysisPlugin/archive/master.zip',
-'date': '2/10/2016',
-'dependencies': [],
-'menu_layout': [('Global Analysis', ['GlobalAnalysis', 'gui'])]}
+<plugin name="Global Analysis">
+	<base_dir>
+		GlobalAnalysis>
+	</base_dir>
+	<date>
+		02/10/2016
+	</date>
+	<author>
+		Brett Settle
+	</author>
+	<description>
+		Analyze the fall and rise times of ROIs by fitting the average trace to a polynomial
+	</description>
+	<url>
+		http://github.com/BrettJSettle/GlobalAnalysisPlugin/archive/master.zip
+	</url>
+	<menu_layout>
+		<menu name="Menu 1">
+			<action location="loc1" function="f1">Action 1</action>
+			<action location="loc2" function="f2">Action 2</action>
+		</menu>
+		<menu name="Menu 2">
+			<action location="loc3" function="f3">Action3</action>
+			<menu name="Submenu">
+				<action location="loc3.5" function="f3.5">Action Here</action>
+			</menu>
+		</menu>
+		<action location="loc4" function="f4">Action 4</action>
+	</menu_layout>
+</plugin>
