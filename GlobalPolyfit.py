ecb54c06129f68629e411c9d5fd7827e42c27342
Brett Settle <brettjsettle@gmail.com>
2016-05-27 20:00:39 -0700

    removed dep check

--- GlobalPolyfit.py
+++ GlobalPolyfit.py
@@ -1,6 +1,5 @@
 
 from __future__ import (absolute_import, division,print_function, unicode_literals)
-import dependency_check
 from future.builtins import (bytes, dict, int, list, object, range, str, ascii, chr, hex, input, next, oct, open, pow, round, super, filter, map, zip)
 import time
 tic=time.time()
